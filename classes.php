4bf6dc44d246fc041428683cb219561093f3faf5
aleksa-radojicic <aleksa.radojicic@outlook.com>
2022-12-06 17:04:07 +0100

    Renamed model to models

--- classes.php
+++ classes.php
@@ -1,10 +1,11 @@
 <?php
 
-require_once('model/db_broker.php');
-require_once('model/order_model.php');
-require_once('model/user_model.php');
-require_once('model/order_item_model.php');
-require_once('model/product_model.php');
+require_once('models/db_broker.php');
+
+require_once('models/order_model.php');
+require_once('models/user_model.php');
+require_once('models/order_item_model.php');
+require_once('models/product_model.php');
 
 require_once('domain/user.php');
 require_once('domain/product.php');
